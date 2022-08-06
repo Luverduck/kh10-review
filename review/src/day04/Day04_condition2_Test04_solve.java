e72066d1d7b750fdc25f5243efd249bdfd0269a1
Luverduck <hyekko92@gmail.com>
2022-08-06 20:39:47 +0900

    Day04_condition2_Test04 : 윤년 계산 - boolean 연산자를 이용한 풀이 - https://floating-branch.tistory.com/28

---
+++ review/src/day04/Day04_condition2_Test04_solve.java
@@ -0,0 +1,48 @@
+package day04;
+
+import java.lang.*;
+
+public class Day04_condition2_Test04_solve {
+
+	public static void main(String[] args) {
+		
+		int year = 2020;
+		int month = 2;
+		
+		int day;
+		switch(month) {
+		case 2:
+			if(year % 4 == 0) {
+				if(year % 100 != 0) {
+					day = 29;
+				}
+				else if(year % 100 == 0){
+					if(year % 400 == 0) {
+						day = 29;
+					}
+					else {
+						day = 28;
+					}
+				}
+				else {
+					day = 28;
+				}
+			}
+			else {
+				day = 28;
+			}
+			break;
+		case 4: case 6: case 9: case 11:
+			day = 30;
+			break;
+		default :
+			day = 31;
+			break;
+		}
+		
+		System.out.println(year + "년 " + month + "월은 총 " + day + "일까지 있습니다");
+		
+		
+	}
+	
+}
