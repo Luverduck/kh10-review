86ee1b1b9bb1eec58fb5fff8b946aba942e2f739
Luverduck <hyekko92@gmail.com>
2022-08-06 20:10:23 +0900

    Day02_data_Test05 : 커피숍 판매 금액 계산 - https://floating-branch.tistory.com/13

---
+++ review/src/day02/Day02_data_Test05.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.lang.*;
+
+public class Day02_data_Test05 {
+
+	public static void main(String[] args) {
+    
+    		int americano = 1300;
+        	int americanoCount = 4;
+        
+        	int latte = 2000;
+        	int latteCount = 3;
+        
+        	int totalPrice = americano * americanoCount + latte * latteCount;
+        
+        	System.out.println(totalPrice);
+    
+    }
+
+}
