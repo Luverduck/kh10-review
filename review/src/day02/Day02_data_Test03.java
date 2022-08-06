d2e56b31ccffe0a74192b5cfb69a8f071b290ff5
Luverduck <hyekko92@gmail.com>
2022-08-06 20:08:17 +0900

    Day02_data_Test03 : 변수의 사용법 - https://floating-branch.tistory.com/7

---
+++ review/src/day02/Day02_data_Test03.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.lang.*;
+
+public class Day02_data_Test03 {
+
+	public static void main(String[] args) {
+		
+        	int jjajang = 5000;
+        	int jjajangCount = 2;
+        
+        	int champon = 6000;
+        	int champonCount = 3;
+        
+        	int totalPrice = jjajang * jjajangCount + champon * champonCount;
+        
+        	System.out.println(totalPrice);
+        
+        }
+
+}
