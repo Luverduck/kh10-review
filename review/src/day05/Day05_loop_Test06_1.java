42f16fbe4b2f8a603565bf53b597e33d41a5b254
Luverduck <hyekko92@gmail.com>
2022-08-06 21:31:43 +0900

    Day05_loop_Test06 : 7의 배수 찾기 + 5가 포함된 숫자 찾기 (for 반복문 활용) - 7의 배수만 출력 - https://floating-branch.tistory.com/38

---
+++ review/src/day05/Day05_loop_Test06_1.java
@@ -0,0 +1,13 @@
+package day05;
+
+public class Day05_loop_Test06_1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i < 100 ; i ++) {
+			if(i % 7 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
