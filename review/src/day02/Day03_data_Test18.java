45175a624e9e9acf3a14f2f90f03135b5e3b492b
Luverduck <hyekko92@gmail.com>
2022-08-06 20:20:35 +0900

    Day03_data_Test18 : 논리 연산자 boolean - https://floating-branch.tistory.com/18

---
+++ review/src/day02/Day03_data_Test18.java
@@ -0,0 +1,20 @@
+package day02;
+
+import java.lang.*;
+
+public class Day03_data_Test18 {
+
+	public static void main(String[] args) {
+
+		int now = 2022;
+		int birth = 2000;
+
+		int age = now - birth + 1;
+
+		boolean purchase = age >= 20;
+
+		System.out.println(purchase);
+
+	}
+
+}
