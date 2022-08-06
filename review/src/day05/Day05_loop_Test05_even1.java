15a0a6d288f1f70418b60096c7c064066d7498d3
Luverduck <hyekko92@gmail.com>
2022-08-06 21:12:50 +0900

    Day05_loop_Test05 : 1부터 100까지 홀수 / 짝수 출력 - 반복수 조건을 직접 조정 - https://floating-branch.tistory.com/37

---
+++ review/src/day05/Day05_loop_Test05_even1.java
@@ -0,0 +1,11 @@
+package day05;
+
+public class Day05_loop_Test05_even1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2 ; i <= 100 ; i = i + 2) {
+			System.out.println(i);
+		}
+	}
+}
