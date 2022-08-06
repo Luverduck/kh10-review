8e992e8b0cdc06525abd4a0c85948ba56b370ca0
Luverduck <hyekko92@gmail.com>
2022-08-06 20:16:06 +0900

    Day02_data_Test13 : 나누기 연산 + 형 변환 - https://floating-branch.tistory.com/16

---
+++ review/src/day02/Day02_data_Test13.java
@@ -0,0 +1,18 @@
+package day02;
+
+import java.lang.*;
+
+public class Day02_data_Test13 {
+
+	public static void main(String[] args){
+    
+    		int money = 50000;
+    		int gasPricePerLiter = 1920;
+    
+    		double gasLiter = (double)money / gasPricePerLiter;
+    
+    		System.out.println(gasLiter);
+    
+    } 
+
+}
