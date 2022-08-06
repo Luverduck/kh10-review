708e78615fa5d49c541c7a06ddcc502a1506ad81
Luverduck <hyekko92@gmail.com>
2022-08-06 21:13:19 +0900

    Day05_loop_Test05 : 1부터 100까지 홀수 / 짝수 출력 - if문을 활용하는 방법 - https://floating-branch.tistory.com/37

---
+++ review/src/day05/Day05_loop_Test05_even2.java
@@ -0,0 +1,13 @@
+package day05;
+
+public class Day05_loop_Test05_even2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i <= 100 ; i ++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
