09d346b5c6de5a3bd4b8e5306a92ae8c105ae637
Luverduck <hyekko92@gmail.com>
2022-08-06 20:52:41 +0900

    Day05_loop_Test02 : 5개의 숫자를 입력으로 받기 (for문과 Scanner 활용) - https://floating-branch.tistory.com/34

---
+++ review/src/day05/Day05_loop_Test02.java
@@ -0,0 +1,17 @@
+package day05;
+
+import java.util.Scanner;
+
+public class Day05_loop_Test02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		for(int i = 0 ; i < 5 ; i ++) {
+			int a = sc.nextInt();
+		}
+	
+		sc.close();
+	}
+}
