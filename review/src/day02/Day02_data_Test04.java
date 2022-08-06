6007f33c90ff3e073940dba235dba79d51896b04
Luverduck <hyekko92@gmail.com>
2022-08-06 20:09:33 +0900

    Day02_data_Test04 : 영화관 요금 계산 - https://floating-branch.tistory.com/12

---
+++ review/src/day02/Day02_data_Test04.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.lang.*;
+
+public class Day02_data_Test04 {
+
+	public static void main(String[] args) {
+    
+    	int adultPrice = 12000;
+        int adultCount = 2;
+        
+        int youthPrice = 7500;
+        int youthCount = 3;
+        
+        int totalPrice = adultPrice * adultCount + youthPrice * youthCount;
+        
+        System.out.println(totalPrice);
+    
+	}
+	
+}
