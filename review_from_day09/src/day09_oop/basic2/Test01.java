902944138472d76138c27b254b570882137d10e2
Luverduck <hyekko92@gmail.com>
2022-08-07 20:47:50 +0900

    Day09_oop.basic2 : 객체 생성 + 출력 메소드 연습 - https://floating-branch.tistory.com/68 - 오타 수정

--- review_from_day09/src/day09_oop/basic2/Test01.java
+++ review_from_day09/src/day09_oop/basic2/Test01.java
@@ -4,21 +4,25 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		
+		// 브라질 객체 생성
 		Country a = new Country();
 		a.rank = 1;
 		a.name = "브라질";
 		a.score = 1828.45;
 		
+		// 벨기에 객체 생성
 		Country b = new Country();
 		b.rank = 2;
 		b.name = "벨기에";
 		b.score = 1823.45;
 		
+		// 프랑스 객체 생성
 		Country c = new Country();
 		c.rank = 3;
-		c.name = "벨기에";
+		c.name = "프랑스";
 		c.score = 1786.15;
 		
+		// 나라 정보 출력
 		a.print();
 		b.print();
 		c.print();
