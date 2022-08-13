bdf906eaaebcd07c414ca30a6711996537d9e4b8
Luverduck <hyekko92@gmail.com>
2022-08-13 13:06:49 +0900

    day09_oop.getter2 : 멤버필드를 새로 정의하지 않고 총합, 평균 구하기

---
+++ review_from_day09/src/day10_oop/getter2/Student.java
@@ -0,0 +1,86 @@
+package day10_oop.getter2;
+
+public class Student {
+
+	// 멤버 필드
+	String name, course;
+	int score, scoreC;
+	
+	// name에 대한 getter & setter
+	String getName() {
+		return this.name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	// course에 대한 getter & setter
+	String course() {
+		return this.course;
+	}
+	
+	void setCourse(String course) {
+		this.course = course;
+	}
+	
+	// score에 대한 getter & setter
+	int getScore() {
+		return this.score;
+	}
+	
+	void setScore(int score) {
+		if(score >= 0 && score <= 100) {		// set을 설정하는 방법 1)
+			this.score = score;
+		}
+	}
+	
+	// scoreC에 대한 getter & setter
+	int getScoreC() {
+		return this.scoreC;
+	}
+	
+	void setScoreC(int scoreC) {
+		if(scoreC < 0 && scoreC > 100) {		// set을 설정하는 방법 2)
+			return;
+		}
+		this.scoreC = scoreC;
+	}
+	
+	// 총합에 대한 getter
+	int getSum() {
+		return this.score + this.scoreC;
+	}
+	
+	// 평균에 대한 getter
+	double getAvg() {
+		return getSum() / 3.0;
+	}
+	
+	// 생성자
+	Student(String name, String course, int score, int scoreC){
+		this.setName(name);
+		this.setCourse(course);
+		this.setScore(score);
+		this.setScoreC(scoreC);
+	}
+	
+	// 출력
+	void print() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("과목 : " + this.course);
+		System.out.println("서술형 점수 : " + this.score);
+		System.out.println("평가자 체크리스트 점수 : " + this.scoreC);
+		System.out.println("총점 : " + getSum());
+		System.out.println("평균 : " + getAvg());
+		System.out.print("통과 여부 : ");
+		if(score >= 40 && scoreC >= 40 && getAvg() >= 60) {
+			System.out.println("통과");
+		}
+		else {
+			System.out.println("재평가");
+		}
+		
+		System.out.println();	// 출력시 구분을 위한
+	}
+}
---
+++ review_from_day09/src/day10_oop/getter2/Test01.java
@@ -0,0 +1,17 @@
+package day10_oop.getter2;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		// 객체 생성
+		Student a = new Student("피카츄", "응용SW기초기술활용", 50, 60);
+		Student b = new Student("라이츄", "응용SW기초기술활용", 40, 80);
+		Student c = new Student("파이리", "프로그래밍언어", 60, 65);
+		
+		// 출력
+		a.print();
+		b.print();
+		c.print();
+	}
+}
