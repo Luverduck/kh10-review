f77859c2774bbe376a29b2bf036daa612966e0ad
Luverduck <hyekko92@gmail.com>
2022-08-13 13:57:07 +0900

    day10_oop.modifier2 : 접근제한을 붙여서 클래스 생성 - https://floating-branch.tistory.com/89

---
+++ review_from_day09/src/day10_oop/modifier2/Student.java
@@ -0,0 +1,98 @@
+package day10_oop.modifier2;
+
+public class Student {
+
+	// 멤버 필드
+	String name;
+	int level, korean, english, math;
+	
+	// getter & setter
+	String getName() {
+		return this.name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
+	int getLevel() {
+		return this.level;
+	}
+	
+	void setLevel(int level) {
+		switch(level) {
+		case 1: case 2: case 3:
+			break;
+		}
+		this.level = level;
+	}
+	
+	int getkorean() {
+		return this.korean;
+	}
+	
+	void setKorean(int korean) {
+		if(korean >= 0 && korean <= 100) {
+			this.korean = korean;
+		}
+	}
+	
+	int getEnglish() {
+		return this.english;
+	}
+	
+	void setEnglish(int english) {
+		if(english >= 0 && english <= 100) {
+			this.english = english;
+		}
+	}
+	
+	int getMath() {
+		return this.math;
+	}
+	
+	void setMath(int math) {
+		if(math >= 0 && math <= 100) {
+			this.math = math;
+		}
+	}
+	
+	int getSum() {
+		return this.korean + this.english + this.korean;
+	}
+	
+	double getAvg() {
+		return getSum() / 3.0;
+	}
+	
+	String getGrade() {
+		if(getAvg() < 70) {
+			return "F";
+		}
+		else if(getAvg() < 80) {
+			return "C";
+		}
+		else if(getAvg() < 90) {
+			return "B";
+		}
+		else {
+			return "A";
+		}
+	}
+	
+	// 생성자
+	Student(String name, int level, int korean, int english, int math){
+		this.setName(name);
+		this.setKorean(korean);
+		this.setEnglish(english);
+		this.setMath(math);
+	}
+	
+	// 출력 메소드
+	void print() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("총점 : " + this.getSum());
+		System.out.println("평균 : " + this.getAvg());
+		System.out.println("등급 : " + this.getGrade());
+	}
+}
