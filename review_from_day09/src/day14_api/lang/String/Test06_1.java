b4ba21eb4851121bf5810b361a2f885650436bfa
Luverduck <hyekko92@gmail.com>
2022-08-15 20:15:59 +0900

    day14_api.lang.String_Test06 : .repeat(int count), .replace(CharSequence target, CharSequence replacement) - 욕설을 욕설의 글자수만큼 별(*)로 치환하는 방법 - https://floating-branch.tistory.com/104

---
+++ review_from_day09/src/day14_api/lang/String/Test06_1.java
@@ -0,0 +1,27 @@
+package day14_api.lang.String;
+
+public class Test06_1 {
+
+	public static void main(String[] args) {
+
+		// 입력
+		String text = "신발2수박3시베리아333이이잉44시방";
+
+		// 필터링 대상의 배열
+		String[] data = new String[] { "신발", "수박", "시베리아", "십장생", "개나리", "주옥", "조카", "시방", "된장", "십자수" };
+
+		// 필터링
+		for (int i = 0; i < data.length; i++) { // i번째 배열의 욕설이 포함되어있는지 확인
+			if (text.contains(data[i])) { 		// 만약 i번째 배열의 욕설이 포함되어 있다면
+				String star = "*";				// 필터링용 별(*) 문자열 선언
+				String filtering = star.repeat(data[i].length());	// 욕설의 길이만큼 별(*) 반복
+				text = text.replace(data[i], filtering);
+				// text 문자열에서 해당 욕설을 ***로 바꾸고
+				// 바뀐 text 문자열을 기존 text 문자열에 덮어쓰기 한다
+			}
+		}
+
+		// 출력
+		System.out.println(text);
+	}
+}
