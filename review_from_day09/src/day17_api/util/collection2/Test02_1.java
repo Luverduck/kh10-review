f770ed5d817b23f963199a2bd730d614466a2f49
Luverduck <hyekko92@gmail.com>
2022-08-21 01:12:00 +0900

    day17_api.util.collection2_Test02 : Set 활용 - 로또 추첨 + Set의 요소 출력 - 확장형 for문을 이용하는 방법 - https://floating-branch.tistory.com/122

---
+++ review_from_day09/src/day17_api/util/collection2/Test02_1.java
@@ -0,0 +1,39 @@
+package day17_api.util.collection2;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test02_1 {
+
+	public static void main(String[] args) {
+		
+		// Set 생성
+		Set<Integer> set = new TreeSet<>();
+		
+		// Random 생성
+		Random r = new Random();
+		
+		// 구현
+		for(int i = 0 ; i < 6 ; i ++) {
+			// 난수 생성
+			int numR = r.nextInt(45) + 1;
+			if(set.contains(numR)) {
+				i --;
+			}
+			else {
+				set.add(numR);
+			}
+		}
+		
+		// 정렬 (오름차순) - TreeSet은 자동으로 오름차순 정렬한다
+		
+		// 출력 - Set은 값을 index로 관리하지 않기 때문에 하나씩 출력이 까다롭다
+		// 2) 확장형 for 구문으로 하는 방법
+		System.out.print("이번주 로또 번호 : ");
+		for(int number : set) {	// set에 있는 데이터 갯수만큼 전부 다
+			System.out.print(number + " ");
+		}
+	}
+}
