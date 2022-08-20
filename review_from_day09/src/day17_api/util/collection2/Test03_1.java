698f7024cfb99aaab6f4282fc0d76dcc972835ea
Luverduck <hyekko92@gmail.com>
2022-08-21 01:31:33 +0900

    day17_api.util.collection2_Test03 : Set 활용 - 좋아요 누르기 + Set의 요소 출력 복습 - 확장형 for문을 이용한 출력 - https://floating-branch.tistory.com/123

---
+++ review_from_day09/src/day17_api/util/collection2/Test03_1.java
@@ -0,0 +1,47 @@
+package day17_api.util.collection2;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test03_1 {
+
+	public static void main(String[] args) {
+		
+		// 글 번호를 저장하는 Set 생성
+		Set<Integer> set = new TreeSet<>();
+		
+		// Scanner 생성
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			// 입력
+			System.out.print("글 번호 입력 : ");
+			int textNum = sc.nextInt();
+			sc.nextLine();
+			
+			// 종료 조건
+			if(textNum == 0) {
+				break;
+			}
+			
+			// 판정
+			if(set.contains(textNum)) {
+				set.remove(textNum);
+				System.out.println(textNum +"번 글에 좋아요를 취소했습니다");
+			}
+			else {
+				set.add(textNum);
+				System.out.println(textNum + "번 글에 좋아요를 눌렀습니다");
+			}
+		}
+		
+		// 2) 확장형 for문을 활용한 Set의 요소 출력
+		System.out.print("좋아요를 누른 글 번호 : ");
+		for(int number : set) {
+			System.out.print(number + "번 ");
+		}
+		
+		sc.close();
+	}
+}
